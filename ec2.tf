a47539295de4814e8ecfa4431273ca55eaadde32
Raviteja <rajaraviteja.marampudi1999@gmail.com>
2025-02-05 23:19:33 +0530

    multi-env

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,14 @@
 resource "aws_instance" "expense"{
-    for_each = var.instance_type
+    for_each = var.instances
     ami = "ami-09c813fb71547fc4f"
     instance_type = each.value
     vpc_security_group_ids = ["sg-0f0063e4192fb0054"]
     tags = merge(
         var.common_tags,
-        var.tags
+        var.tags,
+        {
+            Name = each.key
+        }
     )
 }
 
